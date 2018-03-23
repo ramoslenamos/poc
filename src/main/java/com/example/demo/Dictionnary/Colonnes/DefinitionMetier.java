4a4ccbb582e257292f11bbddf3a6f7fafecd7b03
ramoslenamos <o.wafi@uhp.ac.ma>
2018-03-23 13:24:36 +0100

    ajout de classes pour les colonnes

--- src/main/java/com/example/demo/Dictionnary/Colonnes/DefinitionMetier.java
+++ src/main/java/com/example/demo/Dictionnary/Colonnes/DefinitionMetier.java
@@ -1,7 +1,11 @@
 package com.example.demo.Dictionnary.Colonnes;
 
+import com.example.demo.Dictionnary.Tables.Dictionnary;
+
 /**
  * Created by othmane on 23/03/2018.
  */
 public interface DefinitionMetier {
+    public Definition addInfo(Definition info);
+
 }
---
+++ src/main/java/com/example/demo/Dictionnary/Colonnes/DefinitionMetierImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.Dictionnary.Colonnes;
+
+/**
+ * Created by othmane on 23/03/2018.
+ */
+public class DefinitionMetierImpl {
+
+
+}
