3d518d0866f74795c86c3f76b5bd440f814e213f
axdum <axel.dumont3@gmail.com>
2018-03-28 18:06:17 +0200

    Bug du repository null dans le main résolu

--- src/main/java/com/example/demo/Dictionnary/Tables/UserMetier.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Dictionnary.Tables;
-
-import com.example.demo.EudoNet.JsonEntities.UserInfos;
-
-import java.util.Optional;
-
-public interface UserMetier {
-  public Optional<UserInfos> getUser(Long id);
-}
--- src/main/java/com/example/demo/Dictionnary/Tables/UserRepository.java
+++ src/main/java/com/example/demo/Dictionnary/Tables/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.demo.EudoNet.JsonEntities.UserInfos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserInfos, Long> {
-  UserInfos findByBaseName(String base);
+  UserInfos findOneById(Long id);
 }
